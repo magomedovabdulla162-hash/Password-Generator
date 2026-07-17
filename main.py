6aea91fb7cfb0e5ea97a2656644804f1b438d1b9
Abdulla <magomedovabdulla162@gmail.com>
2026-07-17 23:27:15 +0300

    Implement password generator with user-defined options
    Added password generation logic with customizable character sets.

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+import secrets
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+lowercase = "abcdefghijklmnopqrstuvwxyz"
+
+digits = "0123456789"
+
+special = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/"
+
+if use_uppercase:
+    characters += uppercase
+
+if use_lowercase:
+    characters += lowercase
+
+if use_digits:
+    characters += digits
+
+if use_special:
+    characters += special
+
+password = ""
+for i in range(12):
+    password += secrets.choice(characters)
+print(password)
+
+def get_password_length():
+    length = int(input("Введите длину пароля: "))
+    if length <= 0:
+        print("Длина пароля должна быть больше нуля.")
